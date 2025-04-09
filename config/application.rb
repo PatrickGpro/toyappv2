2438d5618327a48d1bdd8fe1b69a7d2e8be03b3f
PatrickGpro <151019962+PatrickGpro@users.noreply.github.com>
2025-04-09 12:19:21 +0200

    css

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Toyapp2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # 🔽 TO jest dobre miejsce na to:
+    config.i18n.default_locale = :pl
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
